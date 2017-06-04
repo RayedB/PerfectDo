a918a04b733ee7936a883077d87882062b7c55e1
RayedB <rayed.benbrahim@gmail.com>
2017-06-04 23:12:00 +0200

    devise views

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
   # Mailer
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.settings = { address: 'localhost', port: 1025}
-  config.action_mailer.default_url_options = { host: 'local.dev', port: 3000}
+  config.action_mailer.default_url_options = {host: 'local.dev', port: 3000}
 
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170604154542) do
+ActiveRecord::Schema.define(version: 20170604154951) do
 
   create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title",                                             null: false
@@ -41,6 +41,13 @@ ActiveRecord::Schema.define(version: 20170604154542) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "organizations_users", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "user_id",         null: false
+    t.integer "organization_id", null: false
+    t.index ["organization_id", "user_id"], name: "index_organizations_users_on_organization_id_and_user_id", using: :btree
+    t.index ["user_id", "organization_id"], name: "index_organizations_users_on_user_id_and_organization_id", using: :btree
   end
 
   create_table "projects", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
