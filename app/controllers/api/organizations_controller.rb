c17018df709bad061c0559e5d138126973f4e092
Rayed Benbrahim <rayed.benbrahim@gmail.com>
2017-06-20 18:56:01 +0200

    commit

--- app/controllers/api/organizations_controller.rb
+++ app/controllers/api/organizations_controller.rb
@@ -1,9 +1,27 @@
 module Api
   class OrganizationsController < ApplicationController
+
     # GET /organizations
     def index
       @orgs = Organization.all
       render json:@orgs
+    end
+
+    # POST /todos
+    def create
+      @org = Organization.create!(org_params)
+
+    end
+
+    private
+
+    def org_params
+      # whitelist params
+      params.permit(:name)
+    end
+
+    def set_todo
+      @list = Todo.find(params[:id])
     end
 
   end
